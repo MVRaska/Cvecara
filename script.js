9a1dc96b44a51fd0d80f6fb0e74a157d5c8b768c
MVRaska <143351048+MVRaska@users.noreply.github.com>
2025-05-21 21:23:22 +0200

    translate js

--- script.js
+++ script.js
@@ -1,70 +1,69 @@
-let inRuza = document.getElementById('ruza');
-let inLili = document.getElementById('lili');
-let inGerber = document.getElementById('gerber');
+let inRose = document.getElementById('rose');
+let inLily = document.getElementById('lily');
+let inGerbera = document.getElementById('gerbera');
 
-let btnCheck = document.querySelectorAll('input[type="checkbox"]');
-let btnRadioKartica = document.getElementById('kartica');
+let giftCheckboxes = document.querySelectorAll('input[type="checkbox"]');
+let radioCard = document.getElementById('card');
 
-let divIspis = document.getElementById('ispis');
+let outputDiv = document.getElementById('output');
 
+let btnCalculate = document.getElementById('calculate');
+let btnReset = document.getElementById('reset');
 
-let btnIzracunaj = document.getElementById('izracunaj');
-let btnResetuj = document.querySelector("input[type='reset']");
-
-btnIzracunaj.addEventListener('click', (e) => {
+btnCalculate.addEventListener('click', (e) => {
     e.preventDefault();
 
-    let cena = 0;
+    let price = 0;
 
-    let brRuza = inRuza.value;
-    let brLili = inLili.value;
-    let brGerbera = inGerber.value;
+    let numRoses = inRose.value;
+    let numLilies = inLily.value;
+    let numGerberas = inGerbera.value;
     
-    if((brRuza % 1 != 0 || brRuza < 0) || 
-        (brLili % 1 != 0 || brLili < 0) || 
-        (brGerbera % 1 != 0 || brGerbera < 0)) {
-            alert('Pogrešan unos broja cvetova');
-            divIspis.innerHTML = '';
-        } else {
-            divIspis.innerHTML = `<h2>Vaša porudžbina:</h2>`;
-            for(let i = 1; i <= brRuza; i++) {
-                divIspis.innerHTML += `<img src='rose.jpg'>`;
-                cena += 150;
-            }
-            divIspis.innerHTML += '<br>';
-            for(let i = 1; i <= brLili; i++) {
-                divIspis.innerHTML += `<img src='lily.jpg'>`;
-                cena += 120;
-            }
-            divIspis.innerHTML += '<br>';
-            for(let i = 1; i <= brGerbera; i++) {
-                divIspis.innerHTML += `<img src='gerber.jpg'>`;
-                cena += 70;
-            }
-            
-            btnCheck.forEach(box => {
-                if(box.id == 'bombo' && box.checked) {
-                    divIspis.innerHTML += '<br> + bombonjera';
-                    cena += 500;
-                }
-                if(box.id == 'coko' && box.checked) {
-                    divIspis.innerHTML += '<br> + čokolada';
-                    cena += 500;
-                }
-                if(box.id == 'sampanj' && box.checked) {
-                    divIspis.innerHTML += '<br> + šampanjac <br>';
-                    cena += 500;
-                }
-            });
-            divIspis.innerHTML += `<p>Cena bez popusta je: ${cena} din.</p>`;
-
-            if(btnRadioKartica.checked && cena > 2000) {
-                cena = cena * 0.9;
-                divIspis.innerHTML += `<br><p style= 'font-size: 20px;'>Cena sa popustom je: <span>${cena} din<span>.</p>`;
-            }
+    if((numRoses % 1 != 0 || numRoses < 0) || 
+       (numLilies % 1 != 0 || numLilies < 0) || 
+       (numGerberas % 1 != 0 || numGerberas < 0)) {
+        alert('Invalid flower quantity input');
+        outputDiv.innerHTML = '';
+    } else {
+        outputDiv.innerHTML = `<h2>Your order:</h2>`;
+        for(let i = 1; i <= numRoses; i++) {
+            outputDiv.innerHTML += `<img src='rose.jpg'>`;
+            price += 150;
         }
+        outputDiv.innerHTML += '<br>';
+        for(let i = 1; i <= numLilies; i++) {
+            outputDiv.innerHTML += `<img src='lily.jpg'>`;
+            price += 120;
+        }
+        outputDiv.innerHTML += '<br>';
+        for(let i = 1; i <= numGerberas; i++) {
+            outputDiv.innerHTML += `<img src='gerber.jpg'>`;
+            price += 70;
+        }
+        
+        giftCheckboxes.forEach(box => {
+            if(box.id === 'choco_box' && box.checked) {
+                outputDiv.innerHTML += '<br> + box of chocolates';
+                price += 500;
+            }
+            if(box.id === 'chocolate' && box.checked) {
+                outputDiv.innerHTML += '<br> + chocolate';
+                price += 500;
+            }
+            if(box.id === 'champagne' && box.checked) {
+                outputDiv.innerHTML += '<br> + champagne <br>';
+                price += 500;
+            }
+        });
+        outputDiv.innerHTML += `<p>Price without discount: ${price} RSD.</p>`;
+
+        if(radioCard.checked && price > 2000) {
+            price = price * 0.9;
+            outputDiv.innerHTML += `<br><p style='font-size: 20px;'>Price with discount: <span>${price} RSD</span>.</p>`;
+        }
+    }
 });
 
-btnResetuj.addEventListener('click', () => {
-    divIspis.innerHTML = '';
+btnReset.addEventListener('click', () => {
+    outputDiv.innerHTML = '';
 });
